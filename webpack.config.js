14ca3a8772de90582bd16cb23782c647e503294f
evan70 <ivanpaldan@gmail.com>
2020-05-17 22:53:28 +0200

    improve ENV output filenames

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
             '@core': path.resolve(__dirname, 'src/core')
         }
     },
+    devtool: isDev ? 'source-map'
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
